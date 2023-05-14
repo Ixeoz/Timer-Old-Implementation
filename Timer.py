48405ef406b2b89c5145b569d13ab8470ccef0f8
Ixeoz <93093673+Ixeoz@users.noreply.github.com>
2023-05-13 23:17:49 -0500

    Windows 11

--- Timer.py
+++ Timer.py
@@ -1,10 +1,8 @@
 import os
-import shutil
-import urllib.request
-import platform
 import time
 import subprocess
 import ctypes
+import sys
 
 # Obtener el identificador de la ventana actual
 hwnd = ctypes.windll.kernel32.GetConsoleWindow()
@@ -22,10 +20,8 @@ class bcolors:
     WARNING = '\033[93m'
     ENDC = '\033[0m'
 
-# Tamaño de la ventana
 os.system("mode con cols=110 lines=20")
 
-# Interfaz de bienvenida
 print("***********************************************")
 print("*                                             *")
 print("*      Bienvenidos al configurador de Timer.  *")
@@ -36,35 +32,66 @@ print("alguna variación en el temporizador. Por defecto, el Timer está configu
 print("forzar a 0.500ms si se requiere el viejo timer.")
 print(f"{bcolors.OKGREEN}\nEste programa fue creado gracias a Schizobeyond y Amit, de los cuales usamos algunos recursos.{bcolors.ENDC}")
 
-# Tiempo de espera de 5 segundos
 print(f"{bcolors.WARNING}\nEste programa se iniciará en unos segundos...{bcolors.ENDC}")
 time.sleep(5)
-
 print(f"{bcolors.OKGREEN}\n¡Comencemos!{bcolors.ENDC}")
+os.system("cls")
 
-# Obtener información del sistema operativo
-system_info = platform.platform()
+result = subprocess.run('wmic computersystem get PCSystemType', capture_output=True, text=True)
 
-# Verificar si el sistema operativo es Windows 10 o 11
-if "Windows-10" in system_info:
-    print("El sistema operativo es Windows 10.")
+if '2' in result.stdout:
+    print(f"{bcolors.OKGREEN}Este programa no es compatible con Laptops o Notebooks, no es recomendable forzar el temporizador.{bcolors.ENDC}")
+    print(f"{bcolors.WARNING}\nEl programa se cerrará.{bcolors.ENDC}")
     time.sleep(5)
-elif "Windows-11" in system_info:
-    print("El sistema operativo es Windows 11.")
-    time.sleep(5)
-    # Opciones adicionales para Windows 11
-    print(f"{bcolors.WARNING}Seleccione una opción:\n{bcolors.ENDC}")
-    print("1. Agregar GlobalTimerResolutionRequests.")
-    print("2. Eliminar GlobalTimerResolutionRequests.")
-    opcion_11 = input("Ingrese una opción (1 o 2): ")
-    if opcion_11 == "1":
-        os.system('reg.exe add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Kernel" /v "GlobalTimerResolutionRequests" /t REG_DWORD /d "1" /f')
-        print("Se ha agregado la clave GlobalTimerResolutionRequests al registro.")
-    elif opcion_11 == "2":
-        os.system('reg.exe delete "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Kernel" /v "GlobalTimerResolutionRequests" /f')
-        print("Se ha eliminado la clave GlobalTimerResolutionRequests del registro.")
+    sys.exit()
+
+result = subprocess.run('wmic os get Caption', capture_output=True, text=True)
+
+if 'Windows 11' in result.stdout:
+    print(f"{bcolors.OKGREEN}El sistema operativo es Windows 11.{bcolors.ENDC}")
+    
+    if 'GlobalTimerResolutionRequest' in subprocess.run('reg.exe query "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\kernel"', capture_output=True, text=True).stdout:
+        print(f"{bcolors.WARNING}\nADVERTENCIA: {bcolors.ENDC}")
+        print (f"{bcolors.WARNING}\nSi elimina la clave GlobalTimerResolutionRequest, no podrás usar la implementación del viejo Temporizador.{bcolors.ENDC}")
+        opcion_11 = input("\nDesea eliminar la clave GlobalTimerResolutionRequest? (Y/N): ")
+
+        if opcion_11.upper() == "Y":
+            subprocess.run('reg.exe delete "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\kernel" /v "GlobalTimerResolutionRequest" /f', capture_output=True, text=True)
+            print("Se ha eliminado la clave GlobalTimerResolutionRequest del registro.")
+            time.sleep(5)
+            os.system("cls")
+            print(f"{bcolors.WARNING}\nSe requiere reiniciar la PC para que se apliquen los cambios.{bcolors.ENDC}")
+            print("\nLa PC se reiniciará en 10 segundos...")
+            time.sleep(10)
+            os.system('shutdown /r /t 0')
+
+        elif opcion_11.upper() == "N":
+            print(f"{bcolors.OKGREEN}\nLa clave GlobalTimerResolutionRequest no será eliminada.{bcolors.ENDC}")
+            time.sleep(5)
+
+        else:
+            print(f"{bcolors.OKGREEN}Opción no válida. Presione Enter para continuar...{bcolors.ENDC}")
     else:
-        print("Opción no válida.")
+        print(f"{bcolors.WARNING}\nSeleccione una opción: {bcolors.ENDC}")
+        print("\n1. Agregar GlobalTimerResolutionRequest.")
+        opcion_11 = input("\nIngrese una opción (1): ")
+
+        if opcion_11 == "1":
+            subprocess.run('reg.exe add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\kernel" /v "GlobalTimerResolutionRequest" /t REG_DWORD /d "1" /f', capture_output=True, text=True)
+            print(f"{bcolors.OKGREEN}\nSe ha agregado la clave GlobalTimerResolutionRequest al registro.{bcolors.ENDC}")
+            time.sleep(5)
+            os.system("cls")
+            print(f"{bcolors.WARNING}\nSe requiere reiniciar la PC para que se apliquen los cambios.{bcolors.ENDC}")
+            print(f"{bcolors.WARNING}\nVuelve a ejecutar Timer.exe una vez reiniciado.{bcolors.ENDC}")
+            print("\nLa PC se reiniciará en 10 segundos...")
+            time.sleep(10)
+            os.system('shutdown /r /t 0')
+
+        else:
+            print(f"{bcolors.OKGREEN}Opción no válida. Presione Enter para continuar...{bcolors.ENDC}")
+            time.sleep(5)
+else:
+    print("El sistema operativo no es Windows 11.")
 
 # Definir la cadena de opciones
 opciones = "\n1.  5000 (0.5000)\n2.  10000 (1.000)"
@@ -74,20 +101,26 @@ subprocess.Popen("start cmd /k Measure-Sleep.exe", shell=True)
 while True:
     os.system('cls' if os.name == 'nt' else 'clear') # Borrar la pantalla
     print(f"{bcolors.WARNING}Seleccione una opción:\n{bcolors.ENDC}" + opciones)
-    opcion = input("\n> ")
+    opcion = input(f"{bcolors.WARNING}\n> {bcolors.ENDC}")
     
     # Verificar la opción ingresada y ejecutar la acción correspondiente
     if opcion == "1":
-        os.system("cd C:\\ && SetTimerResolution.exe --resolution 5000")
-        print("La resolución se ha establecido en 5000 (0.500).")
-        break # Salir del bucle
+        print(f"{bcolors.OKGREEN}La resolución se ha establecido en 5000 (0.500).{bcolors.ENDC}")
+        print(f"{bcolors.WARNING}\nSe está ejecutando SetTimerResolution.exe, puedes detenerlo en el Administrador de Tareas.{bcolors.ENDC}")
+        os.system("SetTimerResolution.exe --resolution 5000 --no-console")
+        break
     
     elif opcion == "2":
-        os.system("cd C:\\ && SetTimerResolution.exe --resolution 10000")
-        print("La resolución se ha establecido en 10000 (1.000).")
-        break # Salir del bucle
+        print(f"{bcolors.OKGREEN}La resolución se ha establecido en 10000 (1.000).{bcolors.ENDC}")
+        print(f"{bcolors.WARNING}\nSe está ejecutando SetTimerResolution.exe, puedes detenerlo en el Administrador de Tareas.{bcolors.ENDC}")
+        os.system("SetTimerResolution.exe --resolution 10000 --no-console")
+        break
     
     else:
-        # Opción no válida
-        print("Opción no válida. Presione Enter para continuar...")
+        print(f"{bcolors.OKGREEN}Opción no válida. Presione Enter para continuar...{bcolors.ENDC}")
         input()
+
+## Créditos:
+
+## MeasureSleep / SetTimerResolution.exe "Gracias a Amit".
+## Clave del Registro "Gracias a SchizoBeyond".
